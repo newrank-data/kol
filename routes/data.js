46605eddc095ec1bbb45130aa2604f7d391238ac
onedayl <onedayl@Daisy>
2018-07-20 02:51:57 +0800

    1.0:展示、修改、添加

--- routes/data.js
+++ routes/data.js
@@ -1,16 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
-const mongoURL = 'mongodb://localhost:27017';
+const ObjectId = require('mongodb').ObjectId;
+const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_DB = process.env.MONGODB_DB;
 
 router.get('/', (req, res, next) => {
   
-  MongoClient.connect(mongoURL, (err, client) => {
+  MongoClient.connect(MONGODB_URI, { useNewUrlParser: true }, (err, client) => {
 
     const query = {};
     
     if (req.query.name) {
-      const re = eval(`/${req.query.name}/`);
+      const re = eval(`/${req.query.name}/i`);
       query.name = {$regex: re};
     }
   
@@ -29,11 +32,11 @@ router.get('/', (req, res, next) => {
     const skip = req.query.skip ? parseInt(req.query.skip) : 0;
   
     if (!err) {
-      const db = client.db('newrank');
+      const db = client.db(MONGODB_DB);
       const cursor = db.collection('kol').find(query);
       cursor.count((err, num) => {
         if (!err) {
-          cursor.skip(skip).limit(10);
+          cursor.sort({_id: -1}).skip(skip).limit(10);
           cursor.toArray((err, docs) => {
             if (!err) {
               res.status(200).end(JSON.stringify({
@@ -56,11 +59,41 @@ router.get('/', (req, res, next) => {
   });
 });
 
-router.use((req, res) => {
-  res.status(500).end('查询失败');
-  client.close();
+router.post('/', (req, res, next) => {
+  const actionType = req.body.actionType;
+  const id = req.body.id;
+  const item = req.body.item;
+
+  MongoClient.connect(MONGODB_URI, { useNewUrlParser: true }, (err, client) => {
+    const db = client.db(MONGODB_DB);
+
+    if (actionType == 2) {
+      db.collection('kol').updateOne({
+        _id: new ObjectId(id)
+      }, item, (err, reply) => {
+        if (!err) {
+          res.status(200).end('处理成功');
+          client.close();
+        } else {
+          next();
+        }
+      })
+    } else if (actionType == 1) {
+      db.collection('kol').insertOne(item, (err, reply) => {
+        if (!err) {
+          res.status(200).end('处理成功');
+          client.close();
+        } else {
+          next();
+        }
+      });
+    }
+  });
 });
 
-
+router.use((req, res) => {
+  res.status(500).end('处理失败');
+  client.close();
+});
 
 module.exports = router;
